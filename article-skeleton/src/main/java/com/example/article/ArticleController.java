9f091ad6fcba92168576a8ed23ded6c746ceb97e
kdybe01 <kdybe01@gmail.com>
2023-06-21 15:02:35 +0900

    Feat : 게시판 CRUD 컨트롤러 추가 - ArticleController.java

--- article-skeleton/src/main/java/com/example/article/ArticleController.java
+++ article-skeleton/src/main/java/com/example/article/ArticleController.java
@@ -1,27 +1,50 @@
 package com.example.article;
 
+import com.example.article.dto.ArticleDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Slf4j
-// 어노테이션 붙이기
+@RestController
+@RequestMapping("/articles")
 @RequiredArgsConstructor
 public class ArticleController {
     private final ArticleService service;
 
     // POST /articles
-
+    @PostMapping
+    // RESTful한 API는 행동의 결과로 반영된 자원의 상태를 반환함이 옳음
+    public ArticleDto create(@RequestBody ArticleDto dto) {
+        return service.createArticle(dto);
+    }
 
     // GET /articles
-
+    @GetMapping
+    public List<ArticleDto> readAll() {
+        return service.readArticleAll();
+    }
 
     // GET /articles/{id}
-
+    @GetMapping("/{id}")
+    public ArticleDto read(@PathVariable("id") Long id) {
+        return service.readArticle(id);
+    }
 
     // PUT /articles/{id}
-
+    @PutMapping("/{id}")
+    public ArticleDto update(
+            @PathVariable("id") Long id,
+            @RequestBody ArticleDto dto) {
+        return service.updateArticle(id, dto);
+    }
 
     // DELETE /articles/{id}
-
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        service.deleteArticle(id);
+    }
 }
